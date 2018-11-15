d1048011abf8caabae89a97e2816a2410e40b90d
user <user@user-pc>
2018-11-15 12:00:40 +0900

    dd

---
+++ src/child/trip/manager/DataVO.java
@@ -0,0 +1,64 @@
+package child.trip.manager;
+
+public class DataVO {
+	
+	private int page;
+	private String title;
+	private String img;
+	private String roc;
+	private String url;
+	private String tel;
+	private String cate;
+	private String addr;
+	
+	
+	
+	public String getCate() {
+		return cate;
+	}
+	public void setCate(String cate) {
+		this.cate = cate;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getRoc() {
+		return roc;
+	}
+	public void setRoc(String roc) {
+		this.roc = roc;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+}
